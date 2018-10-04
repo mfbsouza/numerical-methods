7f29ee4247efe0019e6688e2eee3ce874edfe6dc
PunishedBois <mfbs@cin.ufpe.br>
2018-10-04 20:36:28 -0300

    finished euler. built simple input method. updated readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# numerical-methods
+# Projeto de Metodos Numericos
+ Programa usando python 3, sympy e matplotlib para aproximação de equações diferencias
+
+## Requerimentos
+ * Python 3
+ * Sympy
+ * Matplotlib
+ Se voce ja tem python 3 instalado no seu Linux, parar instalar as bibliotecas use:
+ ```bash
+ sudo python3 -m pip install -U sympy
+ sudo python3 -m pip install -U matplotlib
+ ```
+## Rodando
+ ```bash
+ python3 main.py
+ ```
+ Exemplos de entrada: (metodo y0 t0 h n funcao)
+ * euler 0 0 0.1 2 1-t+4*y
--- main.py
+++ main.py
@@ -3,16 +3,14 @@ from sympy import *
 # definindo as 'variaveis'
 t, y = symbols("t, y")
 
-
 def main():
-    expr = sympify(input('function: '))
-    y0 = float(input('y0: '))
-    t0 = float(input('t0: '))
-    h = float(input('h: '))
-    n = int(input('n: '))
-    Euler(expr, y0, t0, h, n)
+    # lendo os paramentros como string
+    data = input()
+    words = data.split()
+    if words[0] == 'euler':
+        Euler(float(words[1]), float(words[2]), float(words[3]), int(words[4]), sympify(words[5]))
 
-def Euler(expr, y0, t0, h, n):
+def Euler(y0, t0, h, n, expr):
     # initializing list
     axis_x = []
     axis_y = [y0]
@@ -22,8 +20,12 @@ def Euler(expr, y0, t0, h, n):
         tn = t0 + i*h
         axis_x.append(tn)
         # calculating Yn+1 using Euler (Yn+1 = Yn + h*Fn)
-        Yn1 = axis_y[i] + h*(# cotinue)
-
+        if i != n:
+            Yn1 = axis_y[i] + h*(expr.subs([(y, axis_y[i]), (t, tn)]))
+            axis_y.append(Yn1)
+    print(' x | y')
+    for j in range(n+1):
+        print(axis_x[j], axis_y[j])
 
 # Executa o main do programa
 if __name__ == "__main__":
