119bfacdbad30841615b6384b637cf88ef1ee981
PunishedBois <mfbs@cin.ufpe.br>
2018-10-17 18:49:20 -0300

    finished adam-moulton

--- main.py
+++ main.py
@@ -9,19 +9,50 @@ def main():
     data = input()
     words = data.split()
     if words[0] == 'euler':
-        Euler(float(words[1]), float(words[2]), float(words[3]), int(words[4]), sympify(words[5]))
+        Euler(float(words[1]), float(words[2]), float(words[3]), int(words[4]), sympify(words[5]), 1)
     elif words[0] == 'euler_inverso':
-        EulerInverso(float(words[1]), float(words[2]), float(words[3]), int(words[4]), sympify(words[5]))
+        EulerInverso(float(words[1]), float(words[2]), float(words[3]), int(words[4]), sympify(words[5]),1)
     elif words[0] == 'euler_aprimorado':
-        EulerAprimorado(float(words[1]), float(words[2]), float(words[3]), int(words[4]), sympify(words[5]))
+        EulerAprimorado(float(words[1]), float(words[2]), float(words[3]), int(words[4]), sympify(words[5]),1)
     elif words[0] == 'runge_kutta':
-        rungekutta(float(words[1]), float(words[2]), float(words[3]), int(words[4]), sympify(words[5]))
+        rungekutta(float(words[1]), float(words[2]), float(words[3]), int(words[4]), sympify(words[5]),1)
     elif words[0] == 'adam_bashforth':
         order = int(words[-1])
         ys = []
         for i in range(1, order+1):
             ys.append(float(words[i]))
         AdamBashforth(ys, float(words[order+1]), float(words[order+2]), int(words[order+3]), sympify(words[order+4]), order)
+    elif words[0] == 'adam_bashforth_by_euler':
+        order = int(words[-1])
+        t0 = float(words[2]) + float(words[3])*(order-1)
+        list_y = Euler(float(words[1]), float(words[2]), float(words[3]), (order-1), sympify(words[5]), 0)
+        AdamBashforth(list_y, t0, float(words[3]), int(words[4]), sympify(words[5]), order)
+    elif words[0] == 'adam_bashforth_by_euler_inverso':
+        order = int(words[-1])
+        t0 = float(words[2]) + float(words[3])*(order-1)
+        list_y = EulerInverso(float(words[1]), float(words[2]), float(words[3]), (order-1), sympify(words[5]), 0)
+        AdamBashforth(list_y, t0, float(words[3]), int(words[4]), sympify(words[5]), order)
+    elif words[0] == 'adam_bashforth_by_euler_aprimorado':
+        order = int(words[-1])
+        t0 = float(words[2]) + float(words[3])*(order-1)
+        list_y = EulerAprimorado(float(words[1]), float(words[2]), float(words[3]), (order-1), sympify(words[5]), 0)
+        AdamBashforth(list_y, t0, float(words[3]), int(words[4]), sympify(words[5]), order)
+    elif words[0] == 'adam_bashforth_by_runge_kutta':
+        order = int(words[-1])
+        t0 = float(words[2]) + float(words[3])*(order-1)
+        list_y = rungekutta(float(words[1]), float(words[2]), float(words[3]), (order-1), sympify(words[5]), 0)
+        AdamBashforth(list_y, t0, float(words[3]), int(words[4]), sympify(words[5]), order)
+    elif words[0] == 'adam_moulton':
+        order = int(words[-1])
+        ys = []
+        for i in range(1, order):
+            ys.append(float(words[i]))
+        AdamMoulton(ys, float(words[order]), float(words[order+1]), int(words[order+2]), sympify(words[order+3]), order)
+    elif words[0] == 'adam_moulton_by_euler':
+        order = int(words[-1])
+        t0 = float(words[2]) + float(words[3])*(order - 2)
+        list_y = Euler(float(words[1]), float(words[2]), float(words[3]), (order - 2), sympify(words[5]), 0)
+        AdamMoulton(list_y, t0, float(words[3]), int(words[4]), sympify(words[5]), order)
 
 def ShowGraphic(x, y, title):
     plt.title(title)
@@ -31,10 +62,7 @@ def ShowGraphic(x, y, title):
     plt.plot(x, y, 'k:', color='blue')
     plt.show()
 
-def fn(expr, yn, tn):
-    return expr.subs( [ (t, tn), (y, yn) ] )
-
-def Euler(y0, t0, h, n, expr):
+def Euler(y0, t0, h, n, expr, show):
     # initializing list
     axis_x = []
     axis_y = [y0]
@@ -42,53 +70,60 @@ def Euler(y0, t0, h, n, expr):
     for i in range(n+1):
         # inserting the current Tn in the axis list
         tn = t0 + i*h
-        axis_x.append(round(tn , 3))
+        axis_x.append(tn)
         # calculating Yn+1 using Euler (Yn+1 = Yn + h*Fn)
         if i != n:
             Yn1 = axis_y[i] + h*(expr.subs([(y, axis_y[i]), (t, tn)]))
             # check for backward euler option
-            axis_y.append(round(Yn1, 7))
+            axis_y.append(Yn1)
 
-    print(' x | y')
-    for j in range(n+1):
-        print(axis_x[j], axis_y[j])
-    ShowGraphic(axis_x, axis_y, "Euler Simples")
+    if show == 1:
+        print(' x | y')
+        for j in range(n+1):
+            print(axis_x[j], axis_y[j])
+        ShowGraphic(axis_x, axis_y, "Euler Simples")
+    elif show == 0:
+        return axis_y
 
-def EulerInverso(y0, t0, h, n, expr):
+def EulerInverso(y0, t0, h, n, expr, show):
     #initializing lists
     axis_x = []
     axis_y = [y0]
     for i in range(n+1):
         tn = t0 + i*h
         tn1 = tn + h
-        axis_x.append(round(tn, 3))
+        axis_x.append(tn)
         if i != n:
             #Yn+1 = Yn + h*Fn+1
             Yn1 = solve(axis_y[i]+h*(expr.subs(t, tn1)) - y, implicit=True)
-            axis_y.append(round(Yn1[0], 7))
+            axis_y.append(Yn1[0])
+    if show == 1:
+        for j in range(n+1):
+            print(axis_x[j], axis_y[j])
+        ShowGraphic(axis_x, axis_y, "Euler Inverso")
+    elif show == 0:
+        return axis_y
 
-    for j in range(n+1):
-        print(axis_x[j], axis_y[j])
-    ShowGraphic(axis_x, axis_y, "Euler Inverso")
-
-def EulerAprimorado(y0, t0, h, n, expr):
+def EulerAprimorado(y0, t0, h, n, expr, show):
     #initializing lists
     axis_x = []
     axis_y = [y0]
     for i in range(n+1):
         tn = t0 + i*h
         tn1 = tn + h
-        axis_x.append(round(tn, 3))
+        axis_x.append(tn)
         if i != n:
             #Yn+1 = Yn + (Fn + Fn+1)*h/2
             Yn1 = solve(axis_y[i] + (h/2)*(expr.subs(t, tn1) + expr.subs([(y, axis_y[i]), (t, tn)])) - y, implicit=True)
-            axis_y.append(round(Yn1[0], 7))
+            axis_y.append(Yn1[0])
+    if show == 1:
+        for j in range(n+1):
+            print(axis_x[j], axis_y[j])
+        ShowGraphic(axis_x, axis_y, "Euler Aprimorado")
+    elif show == 0:
+        return axis_y
 
-    for j in range(n+1):
-        print(axis_x[j], axis_y[j])
-    ShowGraphic(axis_x, axis_y, "Euler Aprimorado")
-
-def rungekutta(y0, t0, h, n, expr):
+def rungekutta(y0, t0, h, n, expr, show):
     #initializing lists
     axis_x = []
     axis_y = [y0]
@@ -104,20 +139,23 @@ def rungekutta(y0, t0, h, n, expr):
             # finding Yn+1 by runge-kutta: Yn1 = Yn + (h/6)*(k1 + 2k2 + 2k3 + k4)
             Yn1 = yn + (h/6)*(k1 + 2*k2 + 2*k3 + k4)
             # Saving values into the arrays
-            axis_y.append(round(Yn1, 7))
-        axis_x.append(round(tn, 3))
+            axis_y.append(Yn1)
+        axis_x.append(tn)
 
-    # printing the values
-    for j in range(n+1):
-        print(axis_x[j], axis_y[j])
-    ShowGraphic(axis_x, axis_y, "Runge-Kutta Grau 4")
+    if show == 1:
+        # printing the values
+        for j in range(n+1):
+            print(axis_x[j], axis_y[j])
+        ShowGraphic(axis_x, axis_y, "Runge-Kutta Grau 4")
+    elif show == 0:
+        return axis_y
 
 def AdamBashforth(input_y, t0, h, n, expr, order):
     # initializing lists
     axis_x = []
     axis_y = []
     # Storing the first values
-    aux = order-1
+    aux = order - 1
     for item in input_y:
         axis_y.append(item)
         t_aux = t0 - h*aux
@@ -137,8 +175,8 @@ def AdamBashforth(input_y, t0, h, n, expr, order):
             # Finding Yn+1 by Adam-Bashforth: Yn+1 = Yn + h*(Fn - Fn_1)
             Yn1 = yn + h*(Fn - Fn_1)
             # Saving values into the arrays
-            axis_y.append(round(Yn1, 7))
-            axis_x.append(round((tn+h), 3))
+            axis_y.append(Yn1)
+            axis_x.append((tn+h))
     elif order == 3:
         for i in range(2, n):
             tn = axis_x[i]
@@ -154,8 +192,8 @@ def AdamBashforth(input_y, t0, h, n, expr, order):
             # Finding Yn+1 by Adam-Bashforth:
             Yn1 = yn + h*(Fn - Fn_1 + Fn_2)
             # Saving values into the arrays
-            axis_y.append(round(Yn1, 7))
-            axis_x.append(round((tn+h), 3))
+            axis_y.append(Yn1)
+            axis_x.append((tn+h))
     elif order == 4:
         for i in range(3, n):
             tn = axis_x[i]
@@ -174,8 +212,8 @@ def AdamBashforth(input_y, t0, h, n, expr, order):
             # Finding Yn+1 by Adam-Bashforth:
             Yn1 = yn + h*(Fn - Fn_1 + Fn_2 - Fn_3)
             # Saving values into the arrays
-            axis_y.append(round(Yn1, 7))
-            axis_x.append(round((tn+h), 3))
+            axis_y.append(Yn1)
+            axis_x.append((tn+h))
     elif order == 5:
         for i in range(4, n):
             tn = axis_x[i]
@@ -197,8 +235,8 @@ def AdamBashforth(input_y, t0, h, n, expr, order):
             # Finding Yn+1 by Adam-Bashforth:
             Yn1 = yn + h*(Fn - Fn_1 + Fn_2 - Fn_3 + Fn_4)
             # Saving values into the arrays
-            axis_y.append(round(Yn1, 7))
-            axis_x.append(round((tn+h), 3))
+            axis_y.append(Yn1)
+            axis_x.append((tn+h))
     elif order == 6:
         for i in range(5, n):
             tn = axis_x[i]
@@ -223,8 +261,8 @@ def AdamBashforth(input_y, t0, h, n, expr, order):
             # Finding Yn+1 by Adam-Bashforth:
             Yn1 = yn + h*(Fn - Fn_1 + Fn_2 - Fn_3 + Fn_4 - Fn_5)
             # Saving values into the arrays
-            axis_y.append(round(Yn1, 7))
-            axis_x.append(round((tn+h), 3))
+            axis_y.append(Yn1)
+            axis_x.append((tn+h))
     elif order == 7:
         for i in range(6, n):
             tn = axis_x[i]
@@ -252,8 +290,8 @@ def AdamBashforth(input_y, t0, h, n, expr, order):
             # Finding Yn+1 by Adam-Bashforth:
             Yn1 = yn + h*(Fn - Fn_1 + Fn_2 - Fn_3 + Fn_4 - Fn_5 + Fn_6)
             # Saving values into the arrays
-            axis_y.append(round(Yn1, 7))
-            axis_x.append(round((tn+h), 3))
+            axis_y.append(Yn1)
+            axis_x.append((tn+h))
     elif order == 8:
         for i in range(7, n):
             tn = axis_x[i]
@@ -284,10 +322,183 @@ def AdamBashforth(input_y, t0, h, n, expr, order):
             # Finding Yn+1 by Adam-Bashforth:
             Yn1 = yn + h*(Fn - Fn_1 + Fn_2 - Fn_3 + Fn_4 - Fn_5 + Fn_6 - Fn_7)
             # Saving values into the arrays
-            axis_y.append(round(Yn1, 7))
-            axis_x.append(round((tn+h), 3))
+            axis_y.append(Yn1)
+            axis_x.append((tn+h))
 
     # Priting values
+    for j in range(n+1):
+        print(axis_x[j], axis_y[j])
+    ShowGraphic(axis_x, axis_y, "Adam-Bashforth de ordem: " + str(order))
+
+def AdamMoulton(input_y, t0, h, n, expr, order):
+    # initializing lists
+    axis_x = []
+    axis_y = []
+    # Storing the first values
+    aux = order - 2
+    for item in input_y:
+        axis_y.append(item)
+        t_aux = t0 - h*aux
+        axis_x.append(t_aux)
+        aux = aux - 1
+
+    # Caculating Yn+1
+    if order == 2:
+        for i in range(n):
+            tn = axis_x[i]
+            tn1 = tn + h
+            yn = axis_y[i]
+            # Defining Fn's
+            Fn   = (1/2) * expr.subs( [ (t, tn),   (y, yn)   ] )
+            Fn1  = (1/2) * expr.subs(t, tn1)
+            # Finding Yn+1 by AdamMoulton: Yn+1 = Yn + h*(Fn+1 + Fn)
+            Yn1 = solve(yn + h*(Fn1 + Fn) - y, Implicit=True)
+            # Saving values into the arrays
+            axis_y.append(Yn1[0])
+            axis_x.append(tn1)
+    elif order == 3:
+        for i in range(1, n):
+            tn = axis_x[i]
+            tn1 = tn + h
+            tn_1 = axis_x[i-1]
+            yn = axis_y[i]
+            yn_1 = axis_y[i-1]
+            # Defining Fn's
+            Fn_1 = (1/12) * expr.subs( [ (t, tn_1), (y, yn_1) ] )
+            Fn   = (2/3)  * expr.subs( [ (t, tn),   (y, yn)   ] )
+            Fn1  = (5/12) * expr.subs(t, tn1)
+            # Finding Yn+1 by AdamMoulton: Yn+1 = Yn + h*(Fn+1 + Fn - Fn-1)
+            Yn1 = solve(yn + h*(Fn1 + Fn - Fn_1) - y, Implicit=True)
+            # Saving values into the arrays
+            axis_y.append(Yn1[0])
+            axis_x.append(tn1)
+    elif order == 4:
+        for i in range(2, n):
+            tn = axis_x[i]
+            tn1 = tn + h
+            tn_1 = axis_x[i-1]
+            tn_2 = axis_x[i-2]
+            yn = axis_y[i]
+            yn_1 = axis_y[i-1]
+            yn_2 = axis_y[i-2]
+            # Defining Fn's
+            Fn_2 = (1/24)  * expr.subs( [ (t, tn_2), (y, yn_2) ] )
+            Fn_1 = (5/24)  * expr.subs( [ (t, tn_1), (y, yn_1) ] )
+            Fn   = (19/24) * expr.subs( [ (t, tn),   (y, yn)   ] )
+            Fn1  = (3/8)   * expr.subs(t, tn1)
+            # Finding Yn+1 by AdamMoulton: Yn+1 = Yn + h*(Fn+1 + Fn - Fn-1)
+            Yn1 = solve(yn + h*(Fn1 + Fn - Fn_1 + Fn_2) - y, Implicit=True)
+            # Saving values into the arrays
+            axis_y.append(Yn1[0])
+            axis_x.append(tn1)
+    elif order == 5:
+        for i in range(3, n):
+            tn = axis_x[i]
+            tn1 = tn + h
+            tn_1 = axis_x[i-1]
+            tn_2 = axis_x[i-2]
+            tn_3 = axis_x[i-3]
+            yn = axis_y[i]
+            yn_1 = axis_y[i-1]
+            yn_2 = axis_y[i-2]
+            yn_3 = axis_y[i-3]
+            # Defining Fn's
+            Fn_3 = (19/720)  * expr.subs( [ (t, tn_3), (y, yn_3) ] )
+            Fn_2 = (53/360)  * expr.subs( [ (t, tn_2), (y, yn_2) ] )
+            Fn_1 = (11/30)   * expr.subs( [ (t, tn_1), (y, yn_1) ] )
+            Fn   = (323/360) * expr.subs( [ (t, tn),   (y, yn)   ] )
+            Fn1  = (251/720) * expr.subs(t, tn1)
+            # Finding Yn+1 by AdamMoulton:
+            Yn1 = solve(yn + h*(Fn1 + Fn - Fn_1 + Fn_2 - Fn_3) - y, Implicit=True)
+            # Saving values into the arrays
+            axis_y.append(Yn1[0])
+            axis_x.append(tn1)
+    elif order == 6:
+        for i in range(4, n):
+            tn = axis_x[i]
+            tn1 = tn + h
+            tn_1 = axis_x[i-1]
+            tn_2 = axis_x[i-2]
+            tn_3 = axis_x[i-3]
+            tn_4 = axis_x[i-4]
+            yn = axis_y[i]
+            yn_1 = axis_y[i-1]
+            yn_2 = axis_y[i-2]
+            yn_3 = axis_y[i-3]
+            yn_4 = axis_y[i-4]
+            # Defining Fn's
+            Fn_4 = (3/160)     * expr.subs( [ (t, tn_4), (y, yn_4) ] )
+            Fn_3 = (173/1440)  * expr.subs( [ (t, tn_3), (y, yn_3) ] )
+            Fn_2 = (241/720)   * expr.subs( [ (t, tn_2), (y, yn_2) ] )
+            Fn_1 = (133/240)   * expr.subs( [ (t, tn_1), (y, yn_1) ] )
+            Fn   = (1427/1440) * expr.subs( [ (t, tn),   (y, yn)   ] )
+            Fn1  = (95/288)    * expr.subs(t, tn1)
+            # Finding Yn+1 by AdamMoulton:
+            Yn1 = solve(yn + h*(Fn1 + Fn - Fn_1 + Fn_2 - Fn_3 + Fn_4) - y, Implicit=True)
+            # Saving values into the arrays
+            axis_y.append(Yn1[0])
+            axis_x.append(tn1)
+    elif order == 7:
+        for i in range(5, n):
+            tn = axis_x[i]
+            tn1 = tn + h
+            tn_1 = axis_x[i-1]
+            tn_2 = axis_x[i-2]
+            tn_3 = axis_x[i-3]
+            tn_4 = axis_x[i-4]
+            tn_5 = axis_x[i-5]
+            yn = axis_y[i]
+            yn_1 = axis_y[i-1]
+            yn_2 = axis_y[i-2]
+            yn_3 = axis_y[i-3]
+            yn_4 = axis_y[i-4]
+            yn_5 = axis_y[i-5]
+            # Defining Fn's
+            Fn_5 = (863/60480)   * expr.subs( [ (t, tn_5), (y, yn_5) ] )
+            Fn_4 = (263/2520)    * expr.subs( [ (t, tn_4), (y, yn_4) ] )
+            Fn_3 = (6737/20160)  * expr.subs( [ (t, tn_3), (y, yn_3) ] )
+            Fn_2 = (586/945)     * expr.subs( [ (t, tn_2), (y, yn_2) ] )
+            Fn_1 = (15487/20160) * expr.subs( [ (t, tn_1), (y, yn_1) ] )
+            Fn   = (2713/2520)   * expr.subs( [ (t, tn),   (y, yn)   ] )
+            Fn1  = (19087/60480) * expr.subs(t, tn1)
+            # Finding Yn+1 by AdamMoulton:
+            Yn1 = solve(yn + h*(Fn1 + Fn - Fn_1 + Fn_2 - Fn_3 + Fn_4 - Fn_5) - y, Implicit=True)
+            # Saving values into the arrays
+            axis_y.append(Yn1[0])
+            axis_x.append(tn1)
+    elif order == 8:
+        for i in range(6, n):
+            tn = axis_x[i]
+            tn1 = tn + h
+            tn_1 = axis_x[i-1]
+            tn_2 = axis_x[i-2]
+            tn_3 = axis_x[i-3]
+            tn_4 = axis_x[i-4]
+            tn_5 = axis_x[i-5]
+            tn_6 = axis_x[i-6]
+            yn = axis_y[i]
+            yn_1 = axis_y[i-1]
+            yn_2 = axis_y[i-2]
+            yn_3 = axis_y[i-3]
+            yn_4 = axis_y[i-4]
+            yn_5 = axis_y[i-5]
+            yn_6 = axis_y[i-6]
+            # Defining Fn's
+            Fn_6 = (275/24192)     * expr.subs( [ (t, tn_6), (y, yn_6) ] )
+            Fn_5 = (11351/120960)  * expr.subs( [ (t, tn_5), (y, yn_5) ] )
+            Fn_4 = (1537/4480)     * expr.subs( [ (t, tn_4), (y, yn_4) ] )
+            Fn_3 = (88547/120960)  * expr.subs( [ (t, tn_3), (y, yn_3) ] )
+            Fn_2 = (123133/120960) * expr.subs( [ (t, tn_2), (y, yn_2) ] )
+            Fn_1 = (4511/4480)     * expr.subs( [ (t, tn_1), (y, yn_1) ] )
+            Fn   = (139849/120960) * expr.subs( [ (t, tn),   (y, yn)   ] )
+            Fn1  = (5257/17280)    * expr.subs(t, tn1)
+            # Finding Yn+1 by AdamMoulton:
+            Yn1 = solve(yn + h*(Fn1 + Fn - Fn_1 + Fn_2 - Fn_3 + Fn_4 - Fn_5 + Fn_6) - y, Implicit=True)
+            # Saving values into the arrays
+            axis_y.append(Yn1[0])
+            axis_x.append(tn1)
+
+    # Printing values
     for j in range(n+1):
         print(axis_x[j], axis_y[j])
     ShowGraphic(axis_x, axis_y, "Adam-Bashforth de ordem: " + str(order))
